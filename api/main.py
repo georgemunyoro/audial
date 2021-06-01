8739cbf164cc7f417890fc83f84349bf446e0803
Restyled.io <commits@restyled.io>
2021-06-01 04:13:05 +0000

    Restyled by isort

--- api/main.py
+++ api/main.py
@@ -1,14 +1,13 @@
-import subprocess
 import argparse
-
-from dotenv.main import load_dotenv
-import media
 import os
-from shutil import rmtree
-import db
-import utils
 import pprint
+import subprocess
+from shutil import rmtree
 
+import db
+import media
+import utils
+from dotenv.main import load_dotenv
 
 load_dotenv()
 API_PORT = str(os.getenv("API_PORT"))
